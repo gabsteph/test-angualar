a0a77e28bc8e6a894fcad6cf85c9aa2f452d14a2
gabsteph <gabriel.steph@gmail.com>
2018-11-07 15:43:36 +0100

    ajout du composant grister dans le dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutesModule } from './dashboard.routes';
+import { GridsterModule } from 'angular-gridster2';
+import { DashboardGridsterConfigService } from './dashboard-gridster-config.service';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
-    CommonModule,
-    DashboardRoutesModule
+    CommonModule, 
+    DashboardRoutesModule, 
+    GridsterModule
+  ],
+  providers: [
+    DashboardGridsterConfigService
   ]
 })
 export class DashboardModule { }
